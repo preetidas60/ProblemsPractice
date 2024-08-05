81660961ba7fd318e67a23fd3de3608da64ee371
preetidas60 <dasp69833@gmail.com>
2024-08-05 20:24:44 +0530

    fibonacci

---
+++ Practice/sorting.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public class sorting {
+    public static void main(String[] args) {
+        int[] arr = {5,4,3,2,1};
+        for(int i=1; i<arr.length; i++){
+            int current = arr[i];
+            int j=i-1;
+            while(j>=0 && arr[j] > current){
+                arr[j+1] = arr[j];
+                j--;
+            }
+            arr[j+1] = current;
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
