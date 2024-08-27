f09dd664aa784b45f431bd5213a3e7fc826623d6
preetidas60 <dasp69833@gmail.com>
2024-08-27 15:21:33 +0530

    More pattern techniques

--- Practice/pattern.java
+++ Practice/pattern.java
@@ -1,6 +1,23 @@
 public class pattern {
     public static void main(String[] args) {
-        pattern3(5);
+        pattern5(5);
+    }
+    public static void pattern5(int n){
+        for(int i=1; i<= 2* n; i++){
+            int colsinRow = i > n? n - (i - n): i;
+            for(int j=1; j<=colsinRow; j++){
+                System.out.print("* ");
+            } 
+            System.out.println();
+        }
+    }
+    public static void pattern4(int n){
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
     }
     public static void pattern1(int n){
         for(int i=0; i<n; i++){
