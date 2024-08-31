50d423f584682de973eb4add7796d4900c6bb63d
preetidas60 <dasp69833@gmail.com>
2024-08-31 12:14:52 +0530

    Check whether a number is palindrome or not using recursion

--- Practice/sumOfDigits.java
+++ Practice/sumOfDigits.java
@@ -1,11 +1,12 @@
-public class peak {
+public class sumOfDigits {
     public static void main(String[] args) {
         print(5);
     }
     public static void print(int n){
-        if(n == 0) return;
-
-        print(n-1);
+        if(n == 0){
+            return;
+        } 
         System.out.println(n);
+        print(--n);
     }
 }
